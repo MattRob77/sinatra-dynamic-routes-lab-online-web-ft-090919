6a6becfcd821ff106fdc7354f4a80a671e54b0f0
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-21 19:28:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,12 +1,12 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  
-  get '/reversename/:name' do 
+  # Write your code here!
+  get '/reversename/:name' do
     @name = params[:name]
-    
+
     "#{@name.reverse}"
-  
+  end
     
   
 
